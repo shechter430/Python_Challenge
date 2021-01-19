3d609d9c6c20b6366675326350ddede8bd796b3b
ShondeenH <shon.hechter@gmail.com>
2021-01-18 21:08:29 -0600

    added txt output

--- PyBank/main.py
+++ PyBank/main.py
@@ -17,7 +17,7 @@ with open(py_bank_path, newline='') as csvfile:
     #separating header from list
     header = next(csvreader)
 
-    #loop for rest of the rowsclear
+    #loop for rest of the rows
     for row in csvreader:    
         #creating list of dates and profits/losses
         date.append(row[0])
@@ -25,34 +25,46 @@ with open(py_bank_path, newline='') as csvfile:
         #calculating total profit/losses
         total_profitloss+=int(row[1])
 
-#Print Statements
-print("Financial Analysis")
-print("-"*20)
-
-#total number of months
+#calculating total number of months
 total_months = len(date)
-print(f"Total Months: {(total_months)}")
-print(f"Total: ${(total_profitloss)}")
 
-#Calculating the profit/loss change and finding average
+#Calculating the profit/loss change 
 profit_loss_change_list = []
 for i in range(1,len(profit_loss)):
     profit_loss_change = int(profit_loss[i]) - int(profit_loss[i-1])
     profit_loss_change_list.append(profit_loss_change)
 
+#finding average change in profit/loss
 average_change = sum(profit_loss_change_list)/len(profit_loss_change_list)
 
-    #Print average and greatest increase/decrease
-print(f"Average Change: ${round(average_change,2)}")
-
+#excluding the first month in the date list to properly populate date with greatest increase/decrease
 date.pop(0)
 
+#finding the greatest and the least profit loss
 greatest_increase = max(profit_loss_change_list)
 greatest_decrease = min(profit_loss_change_list)
 
+#finding the associated date with the greatest and the least profit loss
 increasedate_index = profit_loss_change_list.index(greatest_increase)
 decreasedate_index = profit_loss_change_list.index(greatest_decrease)
 
+#print all analysis
+print("Financial Analysis")
+print("-"*20)
+print(f"Total Months: {(total_months)}")
+print(f"Total: ${(total_profitloss)}")
+print(f"Average Change: ${round(average_change,2)}")
 print (f"Greatest Increase in Profits: {date[increasedate_index]} (${greatest_increase})")
 print (f"Greatest Decrease in Profits: {date[decreasedate_index]} (${greatest_decrease}")
 
+#exporting final results to a txt file
+output_path = os.path.join(".", "Analysis", "analysis_summary.txt")
+
+with open(output_path, 'a') as file:
+    file.write(f"Financial Analysis\n")
+    file.write(f"--------------------\n")
+    file.write(f"Total Months: {(total_months)}\n")
+    file.write(f"Total: ${(total_profitloss)}\n")
+    file.write(f"Average Change: ${round(average_change,2)}\n")
+    file.write(f"Greatest Increase in Profits: {date[increasedate_index]} (${greatest_increase})\n")
+    file.write(f"Greatest Decrease in Profits: {date[decreasedate_index]} (${greatest_decrease}\n")
