44706a40997af96fa21d0c3ce6f9e74a3b9079a8
ShondeenH <shon.hechter@gmail.com>
2021-01-18 21:55:24 -0600

    initial push

---
+++ PyPoll/main.py
@@ -0,0 +1,28 @@
+#The total number of votes cast
+#A complete list of candidates who received votes
+#The percentage of votes each candidate won
+#The total number of votes each candidate won
+#The winner of the election based on popular vote.
+
+import os
+import csv
+
+#set path for CSV file
+pypoll_path = os.path.join(".", "Resources", "election_data.csv")
+
+#initialize lists
+voter_id = []
+county = []
+candidate = []
+
+#initialize voter count amount 
+total_votes = 0
+
+#open CSV election data file
+with open(pypoll_path, newline='') as csvfile:
+        csvreader = csv.reader(csvfile, delimiter=",")
+        #separating header 
+        header = next(csvreader)
+
+        
+
