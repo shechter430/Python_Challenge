6928960a4f6bf140a9ef963fe2150d442d2cefd1
ShondeenH <shon.hechter@gmail.com>
2021-01-20 20:58:47 -0600

    fixed variables

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,3 +1,9 @@
+#The total number of votes cast
+#A complete list of candidates who received votes
+#The percentage of votes each candidate won
+#The total number of votes each candidate won
+#The winner of the election based on popular vote.
+
 import os
 import csv
 
@@ -7,12 +13,24 @@ pypoll_path = os.path.join(".", "Resources", "election_data.csv")
 #initialize lists
 county = []
 candidate = []
-
+total_votes = 0
 
 #open CSV election data file
 with open(pypoll_path, newline='') as csvfile:
         csvreader = csv.reader(csvfile, delimiter=",")
         #separating header 
         header = next(csvreader)
+
+        for row in csvreader:
+                total_votes += 1
+
+
+print(f"Total Votes: {total_votes}")
+
+
+
+
+
+
 
 
